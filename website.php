9f4e46437a90d98a34be4a441f9cd59851f61ee8
kumarijyoti95210-design <kumarijyoti95210@gmail.com>
2026-05-12 14:21:36 +0530

    Add files via upload

---
+++ website.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <?php include "nav.php";  ?>
+
+    <div class="background">
+        <h1>Lorem ipsum dolor sit amet <br>consectetur, adipisicing elit. Reiciendis, fugit.</h1>
+    </div>
+
+    
+</body>
+</html>
